d157df86757b0e8e25bc23f33b3032563651162b
Stonewall0210 <76092465+Stonewall0210@users.noreply.github.com>
2021-03-29 18:50:57 +1000

    Fixed Fabric developer environments

--- Chat Logger.py
+++ Chat Logger.py
@@ -8,10 +8,10 @@ print("Opened files")
 
 while True:
     logline=log.readline()
-    if (logline.find('[main/INFO]: [CHAT]') != -1):
+    if (logline.find('[main/INFO]: [CHAT]') != -1 or logline.find("[main/INFO] (Minecraft) [CHAT]") != -1):
         print(logline+"Chat line found! Converting...")
         output.write(logline)
-    elif (logline.find("[main/INFO]: Stopping!") != -1):
+    elif (logline.find("[main/INFO]: Stopping!") != -1 or logline.find("[main/INFO] (Minecraft) Stopping!") != -1):
         print(logline+"Line denotes end of process, stopping!")
         log.close
         output.close
