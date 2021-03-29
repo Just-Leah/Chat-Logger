95701bc8de96e9a6fb861bdeac8af74fb14cacee
Stonewall0210 <76092465+Stonewall0210@users.noreply.github.com>
2021-03-29 17:10:57 +1000

    Uploaded Program

---
+++ Chat Logger.py
@@ -0,0 +1,20 @@
+import sys
+
+print("Chat logger started. It will now search for a file called 'latest.log', and open 'output.log'")
+
+log=open("latest.log", "r")
+output=open("output.log", "w")
+print("Opened files")
+
+while True:
+    logline=log.readline()
+    if (logline.find('[main/INFO]: [CHAT]') != -1):
+        print(logline+"Chat line found! Converting...")
+        output.write(logline)
+    elif (logline.find("[main/INFO]: Stopping!") != -1):
+        print(logline+"Line denotes end of process, stopping!")
+        log.close
+        output.close
+        exit()
+    else:
+        print(logline+"No actions were taken")
